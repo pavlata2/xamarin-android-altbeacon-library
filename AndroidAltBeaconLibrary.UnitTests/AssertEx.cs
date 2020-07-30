06703f1843a5e095d95d528759e1f5a8b0a135cb
Pavel Dvořák <pavel.dvorak@radixal.net>
2020-07-30 19:35:33 +0100

    Add project files.

---
+++ AndroidAltBeaconLibrary.UnitTests/AssertEx.cs
@@ -0,0 +1,68 @@
+﻿using System.Linq;
+using NUnit.Framework;
+
+namespace AndroidAltBeaconLibrary.UnitTests
+{
+	public static class AssertEx
+	{
+		public static void AreEqual(string message, int expected, int actual)
+		{
+			Assert.AreEqual(expected, actual, message);
+		}
+		
+		public static void AreEqual(string message, long expected, long actual)
+		{
+			Assert.AreEqual(expected, actual, message);
+		}
+		
+		public static void AreEqual(string message, byte[] expected, byte[] actual)
+		{
+			Assert.IsTrue(actual.SequenceEqual(expected), message);
+		}
+		
+		public static void AreEqual(string message, double expected, double actual, double delta)
+		{
+			Assert.AreEqual(expected, actual, delta, message);
+		}
+		
+		public static void AreEqual(string message, object expected, object actual)
+		{
+			Assert.AreEqual(expected, actual, message);
+		}
+		
+		public static void AreNotEqual(string message, int expected, int actual)
+		{
+			Assert.AreNotEqual(expected, actual, message);
+		}
+		
+		public static void AreNotEqual(string message, object expected, object actual)
+		{
+			Assert.AreNotEqual(expected, actual, message);
+		}
+		
+		public static void Null(string message, object anObject)
+		{
+			Assert.Null(anObject, message);
+		}
+		
+		public static void NotNull(string message, object anObject)
+		{
+			Assert.NotNull(anObject, message);
+		}
+		
+		public static void True(string message, bool condition)
+		{
+			Assert.True(condition, message);
+		}
+		
+		public static void False(string message, bool condition)
+		{
+			Assert.False(condition, message);
+		}
+		
+		public static void AreNotSame(string message, object expected, object actual)
+		{
+			Assert.AreNotSame(expected, actual, message);
+		}
+	}
+}
---
+++ AndroidAltBeaconLibrary.UnitTests/TestBase.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using Java.IO;
+
+namespace AndroidAltBeaconLibrary.UnitTests
+{
+	public class TestBase
+	{
+		public static byte[] HexStringToByteArray(String s) 
+		{
+			int len = s.Length;
+			var data = new byte[len / 2];
+			for (int i = 0; i < len; i += 2) 
+			{
+				data[i / 2] = (byte)((Convert.ToByte(s.Substring(i, 1), 16) << 4) + (Convert.ToByte(s.Substring(i + 1, 1), 16)));
+			}
+			return data;
+    	}
+    	
+    	public static String ByteArrayToHexString(byte[] bytes) 
+    	{
+	        var sb = new StringBuilder();
+	        for (int i = 0; i < bytes.Length; i++) {
+	            sb.AppendFormat("{0:x2}", bytes[i]);
+	        }
+	        return sb.ToString();
+	    }
+	    
+	    // utilty methods for testing serialization	
+	    protected byte[] ConvertToBytes(Java.Lang.Object obj)
+	    {
+	     //   var bf = new BinaryFormatter();
+		    //using (var ms = new MemoryStream())
+		    //{
+		    //    bf.Serialize(ms, obj);
+		    //    return ms.ToArray();
+		    //}
+		    using(var bos = new MemoryStream())
+		    using(var oos = new Java.IO.ObjectOutputStream(bos))
+		    {
+		    	oos.WriteObject(obj);
+		    	return bos.ToArray();
+		    }
+	    }
+	    
+	    protected Java.Lang.Object ConvertFromBytes(byte[] bytes)
+	    {
+	        using (var ms = new MemoryStream())
+	        using (var ois = new ObjectInputStream(ms))
+		    {
+				//var binForm = new BinaryFormatter();
+				//memStream.Write(bytes, 0, bytes.Length);
+				//memStream.Seek(0, SeekOrigin.Begin);
+				//var obj = binForm.Deserialize(memStream);
+				//return obj;
+				return ois.ReadObject();
+		    }
+	    }
+    }
+}
